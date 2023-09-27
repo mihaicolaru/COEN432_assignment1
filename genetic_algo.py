f398eaa317d6b834a84cd0b4ba32613992642c02
Mihai Olaru <mihai.c.olaru@gmail.com>
2023-09-27 19:29:09 -0400

    initial commit with python file with comments

---
+++ genetic_algo.py
@@ -0,0 +1,21 @@
+# include libraries
+
+
+# define algorithm parameters
+
+# define representation
+
+# fitness test
+# mutation
+# crossover
+
+# define parent selection
+# define survivor selection
+# population size
+# number of generations
+
+
+# generate initial population
+
+
+# output updates
